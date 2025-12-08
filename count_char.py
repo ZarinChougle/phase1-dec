27779c29dbc631ba3c85395d04418b5d7331bfcc
Zarin Chougle <zareenchougle1005@gmail.com>
2025-12-08 15:44:53 +0530

    Added Day 1 DSA practice problems

---
+++ count_char.py
@@ -0,0 +1,3 @@
+s = input("Enter a string: ")
+ch = input("Character to count: ")
+print("Count:", s.count(ch))
---
+++ min_max_list.py
@@ -0,0 +1,3 @@
+arr = list(map(int, input("Enter numbers separated by space: ").split()))
+print("Min:", min(arr))
+print("Max:", max(arr))
